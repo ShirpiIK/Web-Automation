c8963e69505226a6ad2bba0e765151b440fa732a
Shirpi. I <ikshirpi826@gmail.com>
2026-07-11 00:33:15 +0530

    Refactor Chrome driver setup and enhance options
    Removed comments and added an experimental option to detach the browser.

--- web_automation/browser.py
+++ web_automation/browser.py
@@ -8,16 +8,15 @@ def build_chrome_driver(*, headless: bool) -> webdriver.Chrome:
     options = Options()
     options.add_argument("--window-size=1280,900")
     options.add_argument("--disable-notifications")
-    
-    # Idhu dhaan romba mukkiyam! Full page load aaga wait pannama udane adutha step pogum
     options.page_load_strategy = 'eager'
+    
+    # Indha line dhaan script mudinjadhum browser close aagama thadukkum!
+    options.add_experimental_option("detach", True)
 
     if headless:
         options.add_argument("--headless=new")
 
     driver = webdriver.Chrome(options=options)
-    
-    # 30 seconds ku mela load aana automatic aah stop panni error kaatidum
     driver.set_page_load_timeout(30)
     
     return driver
