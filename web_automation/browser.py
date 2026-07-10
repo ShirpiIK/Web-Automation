f22aa78470b869ec2d55775ecbfb605025375d5c
Shirpi. I <ikshirpi826@gmail.com>
2026-07-10 23:40:00 +0530

    Add page load timeout to Chrome driver setup
    Set page load timeout to 30 seconds in Chrome driver.

--- web_automation/browser.py
+++ web_automation/browser.py
@@ -8,8 +8,16 @@ def build_chrome_driver(*, headless: bool) -> webdriver.Chrome:
     options = Options()
     options.add_argument("--window-size=1280,900")
     options.add_argument("--disable-notifications")
+    
+    # Idhu dhaan romba mukkiyam! Full page load aaga wait pannama udane adutha step pogum
+    options.page_load_strategy = 'eager'
 
     if headless:
         options.add_argument("--headless=new")
 
-    return webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=options)
+    
+    # 30 seconds ku mela load aana automatic aah stop panni error kaatidum
+    driver.set_page_load_timeout(30)
+    
+    return driver
